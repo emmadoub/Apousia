23ed814678fe415075a10f991ab2b3fb92abbd9a
emmadoub <emma.dbld@gmail.com>
2023-04-13 00:57:33 +0300

    redirect apousia.com

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
 const port = 8080;
 
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  if (req.headers.host === 'apousia.com') {
+    return res.redirect(301, 'https://www.apousia.com' + req.url);
+  }
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
